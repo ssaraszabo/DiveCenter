b287e1001f0e6046530c147aec27cf5af487b5d1
Szabo Sara <szabosara.a1@gmail.com>
2024-11-11 11:48:04 +0200

    Person Class

---
+++ src/Person.java
@@ -0,0 +1,50 @@
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+    private String contactInfo;
+
+    public Person(int id, String name, int age, String contactInfo) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.contactInfo = contactInfo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [id=" + id + ", name=" + name + ", age=" + age + ", contactInfo=" + contactInfo + "]";
+    }
+}
