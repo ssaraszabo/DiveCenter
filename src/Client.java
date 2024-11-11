5e45d9ae156ead4b16fed9c96661d74a2dcab3c9
Szabo Sara <szabosara.a1@gmail.com>
2024-11-11 11:46:17 +0200

    Client Class

---
+++ src/Client.java
@@ -0,0 +1,31 @@
+public class Client extends Person {
+    private String experienceLevel;
+    private boolean isMember;
+
+    public Client(int id, String name, int age, String contactInfo, String experienceLevel, boolean isMember) {
+        super(id, name, age, contactInfo);
+        this.experienceLevel = experienceLevel;
+        this.isMember = isMember;
+    }
+
+    public String getExperienceLevel() {
+        return experienceLevel;
+    }
+
+    public void setExperienceLevel(String experienceLevel) {
+        this.experienceLevel = experienceLevel;
+    }
+
+    public boolean isMember() {
+        return isMember;
+    }
+
+    public void setMember(boolean isMember) {
+        this.isMember = isMember;
+    }
+
+    @Override
+    public String toString() {
+        return "Client [experienceLevel=" + experienceLevel + ", isMember=" + isMember + ", " + super.toString() + "]";
+    }
+}
