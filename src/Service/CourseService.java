67b80b1acad1ed6b825475c3b534a4276587aae2
Szabo Sara <szabosara.a1@gmail.com>
2024-11-19 13:38:03 +0200

    courserepo update

--- src/Service/CourseService.java
+++ src/Service/CourseService.java
@@ -1,34 +1,41 @@
 package Service;
-import Domain.Equipment;
 import Repository.CourseRepository;
-import java.util.Date;
+
 import java.util.List;
 import Domain.Course;
+import Repository.IRepository;
 
 public class CourseService {
-    private final CourseRepository courseRepository;
+    private final IRepository<Course> courseRepository;
 
-    public CourseService(CourseRepository courseRepository) {
+    public CourseService(IRepository<Course> courseRepository) {
         this.courseRepository = courseRepository;
     }
 
-    public Course getCourseById(int courseId) {
-        for (Course course : courseRepository.getAllCourses()) {
-            if (course.getCourseID() == courseId) {
-                return course;
-            }
-        }
-        System.out.println("Course with ID " + courseId + " not found.");
-        return null;
-    }
-
+//    public Course getCourseById(int courseId) {
+//        for (Course course : courseRepository.getAllCourses()) {
+//            if (course.getCourseID() == courseId) {
+//                return course;
+//            }
+//        }
+//        System.out.println("Course with ID " + courseId + " not found.");
+//        return null;
+//    }
+//
+//    public List<Course> getAllCourses() {
+//        return courseRepository.getAllCourses();
+//    }
+//
+//    public boolean hasAvailableSpots(int courseId) {
+//        Course course = courseRepository.getCourseById(courseId);
+//        return course != null && course.getCurrentCapacity() < course.getMaxCapacity();
+//    }
     public List<Course> getAllCourses() {
-        return courseRepository.getAllCourses();
+        return courseRepository.readAll();
     }
 
-    public boolean hasAvailableSpots(int courseId) {
-        Course course = courseRepository.getCourseById(courseId);
-        return course != null && course.getCurrentCapacity() < course.getMaxCapacity();
+    public Course getCourseById(int courseId) {
+        return courseRepository.read(courseId);
     }
 }
 
